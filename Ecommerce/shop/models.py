6773e0c859a6d6a51fc4eb05b37191521b7fc558
gaetan <tangoua8@gmail.com>
2025-12-21 13:23:23 +0100

    ajout de category et projet

--- Ecommerce/shop/models.py
+++ Ecommerce/shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-date_added']
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='categories', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.CharField(max_length=500)
+    date_added = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-date_added']
