88f78063878d4eba92cf65d172476ce30d7a7aec
gaetan <tangoua8@gmail.com>
2025-12-25 06:12:51 +0100

    augmentation de la pagination et finalisation de la barre de recherche

--- Ecommerce/shop/views.py
+++ Ecommerce/shop/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     product_objects = Product.objects.all()
+    item_name = request.GET.get('item-name', None)
+    
+    if item_name:
+        product_objects = Product.objects.filter(title__icontains=item_name)
+
+    paginator = Paginator(product_objects, 4)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+    
     return render(request, 'shop/index.html', {'products_objects': product_objects})
